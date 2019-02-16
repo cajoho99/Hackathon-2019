9f26be48265d81e783073d571745cc0d6eed4bc9
Carl Holmberg <carl.j.holmberg@telia.com>
2019-02-16 16:23:38 +0100

    get posts

--- backend/getPost.php
+++ backend/getPost.php
@@ -2,12 +2,34 @@
 <?php
     //function getPost($id, $course)
 
-    $query = "SELECT * FROM post WHERE id = :id AND course = :course";
-    $stmt = $DB->getInstance()->prepare($query);
-    $stmt->bindParm(':id',$_POST['id']);
-    $stmt->bindParm(':course',$_POST['course']);
+    //$query = "SELECT * FROM post WHERE id = :id AND course = :course";
+    //$stmt = $DB->getInstance()->prepare($query);
+    //$stmt->bindParm(':id',$_POST['id']);
+    //$stmt->bindParm(':course',$_POST['course']);
 
+    //$stmt->execute();
+
+    //echo $stmt->fetch();
+
+include 'connectDB.php';
+
+function getPost($course, $id)
+{
+    $stmt = DB::getInstance()->prepare("SELECT title, score, content FROM posts WHERE course = :course AND id = :id");
+    $stmt->bindParam(":course", $course);
+    $stmt->bindParam(":id", $id);
     $stmt->execute();
+    $res = $stmt->fetchAll();
+    $out = "";
+    foreach ($res as $row) {
 
-    echo $stmt->fetch();
+        $out = $row[0] . "," . $row[1] . "," . $row[2] . ",";
+    }
+
+    return $out;
+}
+$t = getPost($_POST['course'], $_POST['id']);
+echo ($t);
+
+
 ?>
